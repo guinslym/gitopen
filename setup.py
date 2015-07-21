45b4d4811244004f28abc700df93a055efc11612
Guinsly Mondesir <guinslym @gmail.com>
2015-07-21 09:37:45 -0400

    adding task

--- setup.py
+++ setup.py
@@ -16,10 +16,8 @@ setup(
 
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
 
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4'        
